7acbe511e3a400a81f403c20ac7dc15bcee13d7b
Marco Sarkady <s2010458016@students.fh-hagenberg.at>
2024-08-26 21:43:30 +0200

    added responsive sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import Sidebar, { SidebarItem } from "./components/sidebar/sidebar";
+import { IoPersonSharp } from "react-icons/io5";
+import { MdMedicationLiquid } from "react-icons/md";
+import { GiMedicalDrip } from "react-icons/gi";
+import { MdCoronavirus } from "react-icons/md";
+import { PiClockCounterClockwiseBold } from "react-icons/pi";
+import { MdFormatListBulleted } from "react-icons/md";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex">
+      <Sidebar>
+        <SidebarItem 
+          icon={<IoPersonSharp size={25} />}
+          text="Patients"
+          alert={false}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <SidebarItem 
+          icon={<MdMedicationLiquid size={25} />}
+          text="Medications"
+          alert={false}
+        />
+        <SidebarItem 
+          icon={<GiMedicalDrip size={25} />}
+          text="Allergies & Intolerances"
+          alert={false}
+        />
+        <SidebarItem 
+          icon={<MdCoronavirus size={25} />}
+          text="History of Past Illnesses"
+          alert={false}
+        />
+        <SidebarItem 
+          icon={<PiClockCounterClockwiseBold size={25} />}
+          text="Active Problems"
+          alert={false}
+        />
+        <SidebarItem 
+          icon={<MdFormatListBulleted size={25} />}
+          text="Plan of Treatment"
+          alert={false}
+        />
+      </Sidebar>
+      <main className="flex-1 p-6 ml-64">
+        <h1 className="text-2xl">Welcome to the Page</h1>
+        <p>Your content goes here.</p>
+      </main>
+    </div>
   );
 }
