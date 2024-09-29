aa897d74e8acfada931bcae9c46925c2a7308d56
Robin Berger <robin.berger@gmx.at>
2024-09-29 18:02:54 +0200

    added input fields for api input

--- src/app/components/sidebar/input-options/ApiInput.tsx
+++ src/app/components/sidebar/input-options/ApiInput.tsx
@@ -1,5 +1,29 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function ApiInput() {
-  return <p>API Input</p>;
+  const [identifier, setIdentifier] = useState('');
+  const [system, setSystem] = useState('');
+  // TODO: Where do we want to fetch data from the server? This should probably not happen every time the system or identifier changes, which would be on every sign that gets typed in
+  return (
+    <>
+      <input
+        value={system}
+        placeholder={'Enter system value ...'}
+        onChange={(e) => setSystem(e.target.value)}
+        className={
+          'h-8 border-2 border-gray-500 rounded-md p-2 my-1 w-full resize-none'
+        }
+      />
+      <input
+        value={identifier}
+        placeholder={'Enter identifier ...'}
+        onChange={(e) => setIdentifier(e.target.value)}
+        className={
+          'h-8 border-2 border-gray-500 rounded-md p-2 my-1 w-full resize-none'
+        }
+      />
+    </>
+  );
 }
