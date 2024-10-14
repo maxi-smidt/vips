925eb1bb0131741846043818265524d6eaa2db78
msmidt <maxismidt@icloud.com>
2024-10-14 18:09:13 +0200

    [Feature #35] customization

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,48 +4,48 @@ import { InputIcon } from 'primereact/inputicon';
 import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { Button } from 'primereact/button';
+import 'primeicons/primeicons.css';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="bg-stone-100">
       <div className="p-4 flex items-center justify-between h-16">
         <div className="flex items-center">
-          <Image
-            src={`${process.env.IMAGE_PATH}/raccoon-forwards.png`}
-            alt="HAPI FHIR Logo"
-            className="h-12 w-12"
-            width={266}
-            height={293}
-          />
+          <Link href="/">
+            <Image
+              src={`${process.env.IMAGE_PATH}/raccoon-forwards.png`}
+              alt="HAPI FHIR Logo"
+              className="h-12 w-12"
+              width={266}
+              height={293}
+            />
+          </Link>
           <span className="ml-4 text-2xl font-semibold text-gray-800">
             VIPS
           </span>
         </div>
         <div className="flex items-center mr-3 gap-3">
-          <div>
-            <div className="relative">
-              <IconField iconPosition="left">
-                <InputIcon>
-                  <Image
-                    src={`${process.env.IMAGE_PATH}/icons/search.svg`}
-                    alt="Search"
-                    width={20}
-                    height={20}
-                  />
-                </InputIcon>
-                <InputText placeholder="Search" />
-              </IconField>
-            </div>
-          </div>
-
-          <Button className="ml-auto" severity="secondary" text>
-            <Image
-              src={`${process.env.IMAGE_PATH}/icons/file_pdf.svg`}
-              alt="Pdf export"
-              width={20}
-              height={20}
+          <Link href="/customize">
+            <Button
+              icon="pi pi-objects-column"
+              label="Customize"
+              outlined
+              severity="secondary"
             />
-          </Button>
+          </Link>
+
+          <IconField iconPosition="left">
+            <InputIcon className="pi pi-search" />
+            <InputText placeholder="Search" />
+          </IconField>
+
+          <Button
+            className="ml-auto"
+            severity="secondary"
+            outlined
+            icon="pi pi-file-pdf"
+          />
         </div>
       </div>
     </header>
---
+++ src/app/customize/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-3 divide-x">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+    </div>
+  );
+}
