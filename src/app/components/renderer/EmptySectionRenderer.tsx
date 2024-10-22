5db09b95c5fc0be3b37640c69c1685ba56cd3e7a
1anja1 <109726549+1anja1@users.noreply.github.com>
2024-10-22 21:51:45 +0200

    [Feature #76] add rendering for empty sections without resources (#88)
    * 76 add rendering for empty sections without resources
    
    * [Feature #76] EmptySectionRenderer
    
    ---------
    
    Co-authored-by: msmidt <maxismidt@icloud.com>

---
+++ src/app/components/renderer/EmptySectionRenderer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EmptySectionRenderer() {
+  return (
+    <div className="p-2 flex justify-center">
+      <p>No entries available for this section.</p>
+    </div>
+  );
+}
