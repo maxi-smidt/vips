1f959c211cd4f98b364f8e64ada4a389b7b84b10
Maximilian Smidt <134879273+maxi-smidt@users.noreply.github.com>
2024-10-27 17:15:15 +0100

    [NOISSUE] added RendererMap (#129)

--- src/app/components/renderer/EntryRenderer.tsx
+++ src/app/components/renderer/EntryRenderer.tsx
@@ -1,17 +1,8 @@
 import { ConfigEntry } from '@/app/types/Config';
-import { RendererType } from '@/app/types/RendererType';
-import IdentifierRenderer from '@/app/components/renderer/fhir/IdentifierRenderer';
-import HumanNameRenderer from '@/app/components/renderer/fhir/HumanNameRenderer';
-import DefaultRenderer from '@/app/components/renderer/fhir/DefaultRenderer';
-import CodeableConceptRenderer from '@/app/components/renderer/fhir/CodeableConceptRenderer';
-import AddressRenderer from '@/app/components/renderer/fhir/AddressRenderer';
-import CodingRenderer from '@/app/components/renderer/fhir/CodingRenderer';
-import ContactPointRenderer from '@/app/components/renderer/fhir/ContactPointRenderer';
+import { rendererMap } from '@/app/types/RendererType';
 import { useBundle } from '@/app/hooks/useBundle';
-import LinkRenderer from '@/app/components/renderer/fhir/LinkRenderer';
 import { Resource } from '@smile-cdr/fhirts/dist/FHIR-R4/classes/resource';
 import { ResourceUtils } from '@/app/utils/ResourceUtils';
-import AnnotationRenderer from '@/app/components/renderer/fhir/AnnotationRenderer';
 
 const resourceUtils = new ResourceUtils();
 
@@ -29,39 +20,18 @@ export default function EntryRenderer({
     ? getReference()
     : getNoneReference(configEntry.path);
 
-  console.log(configEntry, values);
+  const RendererComponent = rendererMap[configEntry.renderer];
 
-  const getRenderer = (value: unknown) => {
-    switch (configEntry.renderer) {
-      case RendererType.DEFAULT:
-        return <DefaultRenderer value={value} configEntry={configEntry} />;
-      case RendererType.HUMAN_NAME:
-        return <HumanNameRenderer value={value} configEntry={configEntry} />;
-      case RendererType.IDENTIFIER:
-        return <IdentifierRenderer value={value} configEntry={configEntry} />;
-      case RendererType.ADDRESS:
-        return <AddressRenderer value={value} configEntry={configEntry} />;
-      case RendererType.CODEABLE_CONCEPT:
-        return (
-          <CodeableConceptRenderer value={value} configEntry={configEntry} />
-        );
-      case RendererType.CODING:
-        return <CodingRenderer value={value} configEntry={configEntry} />;
-      case RendererType.CONTACT_POINT:
-        return <ContactPointRenderer value={value} configEntry={configEntry} />;
-      case RendererType.LINK:
-        return <LinkRenderer value={value} configEntry={configEntry} />;
-      case RendererType.ANNOTATION:
-        return <AnnotationRenderer value={value} configEntry={configEntry} />;
-      default:
-        throw new Error(`Unknown renderer type: ${configEntry.renderer}`);
-    }
-  };
+  if (!RendererComponent) {
+    throw new Error(`Unknown renderer type: ${configEntry.renderer}`);
+  }
 
   return (
     <div id={configEntry.path}>
       {values.map((value, index) => (
-        <div key={`${configEntry.path}-${index}`}>{getRenderer(value)}</div>
+        <div key={`${configEntry.path}-${index}`}>
+          {<RendererComponent value={value} configEntry={configEntry} />}
+        </div>
       ))}
     </div>
   );
--- src/app/types/RendererType.ts
+++ src/app/types/RendererType.ts
@@ -1,3 +1,13 @@
+import AnnotationRenderer from '@/app/components/renderer/fhir/AnnotationRenderer';
+import DefaultRenderer from '@/app/components/renderer/fhir/DefaultRenderer';
+import IdentifierRenderer from '@/app/components/renderer/fhir/IdentifierRenderer';
+import HumanNameRenderer from '@/app/components/renderer/fhir/HumanNameRenderer';
+import AddressRenderer from '@/app/components/renderer/fhir/AddressRenderer';
+import CodeableConceptRenderer from '@/app/components/renderer/fhir/CodeableConceptRenderer';
+import CodingRenderer from '@/app/components/renderer/fhir/CodingRenderer';
+import ContactPointRenderer from '@/app/components/renderer/fhir/ContactPointRenderer';
+import LinkRenderer from '@/app/components/renderer/fhir/LinkRenderer';
+
 export enum RendererType {
   ANNOTATION = 'AnnotationRenderer',
   DEFAULT = 'DefaultRenderer',
@@ -9,3 +19,15 @@ export enum RendererType {
   CONTACT_POINT = 'ContactPointRenderer',
   LINK = 'LinkRenderer',
 }
+
+export const rendererMap = {
+  [RendererType.ANNOTATION]: AnnotationRenderer,
+  [RendererType.DEFAULT]: DefaultRenderer,
+  [RendererType.IDENTIFIER]: IdentifierRenderer,
+  [RendererType.HUMAN_NAME]: HumanNameRenderer,
+  [RendererType.ADDRESS]: AddressRenderer,
+  [RendererType.CODEABLE_CONCEPT]: CodeableConceptRenderer,
+  [RendererType.CODING]: CodingRenderer,
+  [RendererType.CONTACT_POINT]: ContactPointRenderer,
+  [RendererType.LINK]: LinkRenderer,
+};
