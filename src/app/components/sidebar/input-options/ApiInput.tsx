8a463dcf7e1a9b53c44bb9458206c0a8844b0e10
Anja <anja.schwab939@gmail.com>
2024-10-28 22:19:36 +0100

    add api upload

--- src/app/components/sidebar/input-options/ApiInput.tsx
+++ src/app/components/sidebar/input-options/ApiInput.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { useBundle } from '@/app/hooks/useBundle';
 
 // access .env api -> or better expose variable in next config
 // const API_URL = process.env.FHIR_API;
@@ -12,25 +13,46 @@ import { InputText } from 'primereact/inputtext';
 export default function ApiInput() {
   const [identifier, setIdentifier] = useState<string>('');
   const [system, setSystem] = useState<string>('');
+  const { setBundle } = useBundle();
 
+  const loadIpsFromApi = async () => {
+    try {
+      const response = await fetch(
+        `https://server.fire.ly/Bundle?composition.patient.identifier=${identifier}`,
+        {
+          method: 'GET',
+        },
+      );
+      console.log(response);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await response.json();
+      setBundle(data);
+    } catch (error) {
+      console.error('Error loading data:', error);
+      alert('The data could not be fetched or parsed');
+    }
+  };
   return (
     <>
       <InputText
         value={system}
         onChange={(e) => setSystem(e.target.value)}
-        placeholder="Enter system value ..."
+        placeholder="Enter fhir server endpoint ..."
       />
 
       <InputText
         value={identifier}
         onChange={(e) => setIdentifier(e.target.value)}
-        placeholder="Enter identifier ..."
+        placeholder="Enter SVNR ..."
       />
 
       <Button
         label="Load IPS"
         severity="secondary"
-        onClick={() => {}}
+        onClick={loadIpsFromApi}
         disabled={!identifier || !system}
       />
     </>
