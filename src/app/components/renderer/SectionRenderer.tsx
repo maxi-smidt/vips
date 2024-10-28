05804bef7fcf2ca92bf055e0d2ea5b59dc4c6688
msmidt <maxismidt@icloud.com>
2024-10-28 12:47:51 +0100

    [Feature #77] hide empty sub sections

--- src/app/components/renderer/SectionRenderer.tsx
+++ src/app/components/renderer/SectionRenderer.tsx
@@ -1,8 +1,9 @@
 import { ConfigSection } from '@/app/types/Config';
-import React, {ReactNode} from 'react';
+import React from 'react';
 import ComponentRenderer from '@/app/components/renderer/ComponentRenderer';
 import { v4 as uuidv4 } from 'uuid';
 import { Resource } from '@smile-cdr/fhirts/dist/FHIR-R4/classes/resource';
+import { renderToString } from 'react-dom/server';
 
 interface SectionRendererProps {
   configSection: ConfigSection;
@@ -10,43 +11,41 @@ interface SectionRendererProps {
   resource: Resource;
 }
 
-function hasTextNodeInReactNode(node: ReactNode): boolean {
-  // If the node is a string or number, it is a text node
-  if (typeof node === 'string' || typeof node === 'number') {
-    return true;
-  }
-
-  // If the node is an array, check each item
-  if (Array.isArray(node)) {
-    return node.some(hasTextNodeInReactNode);
-  }
-
-  // If the node is a React element, check its children
-  if (React.isValidElement(node)) {
-    return hasTextNodeInReactNode(node.props.children);
-  }
-
-  // If the node is none of the above, return false
-  return false;
-}
+const isEmptyDiv = (component: React.JSX.Element) => {
+  const rendered = renderToString(component);
+  const emptyDivRegex = /^<div(?: [^>]*)?>\s*<\/div>$/;
+  return emptyDivRegex.test(rendered);
+};
 
 export default function SectionRenderer({
   configSection,
   depth,
   resource,
 }: SectionRendererProps) {
-  const checkIfEmptyList = configSection.components.map((component) => (
-    if(){
+  const sections = configSection.components.map((component) => {
+    return (
+      <ComponentRenderer
+        key={uuidv4()}
+        configComponent={component}
+        resource={resource}
+        depth={depth + 1}
+      />
+    );
+  });
 
-    }
-  ));
-  return (
-    <div
-      key={configSection.display}
-      className={`p-2 bg-gray-${100 * depth} rounded-xl ${depth == 0 && 'flex flex-col gap-2'}`}
-    >
-      {configSection.display && <h3>{configSection.display}</h3>}
-      {checkIfEmptyList}
-    </div>
-  );
+  const filteredSections = sections.filter((section) => !isEmptyDiv(section));
+
+  if (filteredSections.length > 0) {
+    return (
+      <div
+        key={configSection.display}
+        className={`p-2 bg-gray-${100 * depth} rounded-xl ${depth == 0 && 'flex flex-col gap-2'}`}
+      >
+        {configSection.display && <h3>{configSection.display}</h3>}
+        {filteredSections}
+      </div>
+    );
+  }
+
+  return null;
 }
