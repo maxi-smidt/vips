4198d8a61564b11d22710c885f185d54ceadea3b
Maximilian Smidt <134879273+maxi-smidt@users.noreply.github.com>
2024-10-28 12:49:40 +0100

    [Feature #77] hide empty subsections (#150)
    * hide accordions if section is empty
    
    * Revert "hide accordions if section is empty"
    
    This reverts commit 25b25b2c0d93859453f72ec171f224cd53134b0b.
    
    * unfinished bugfix for maxi
    
    * [Feature #77] hide empty sub sections
    
    ---------
    
    Co-authored-by: Marco Sarkady <s2410595013@students.fh-hagenberg.at>
    Co-authored-by: robinb00 <robin.berger@gmx.at>

--- src/app/components/renderer/SectionRenderer.tsx
+++ src/app/components/renderer/SectionRenderer.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ComponentRenderer from '@/app/components/renderer/ComponentRenderer';
 import { v4 as uuidv4 } from 'uuid';
 import { Resource } from '@smile-cdr/fhirts/dist/FHIR-R4/classes/resource';
+import { renderToString } from 'react-dom/server';
 
 interface SectionRendererProps {
   configSection: ConfigSection;
@@ -10,25 +11,41 @@ interface SectionRendererProps {
   resource: Resource;
 }
 
+const isEmptyDiv = (component: React.JSX.Element) => {
+  const rendered = renderToString(component);
+  const emptyDivRegex = /^<div(?: [^>]*)?>\s*<\/div>$/;
+  return emptyDivRegex.test(rendered);
+};
+
 export default function SectionRenderer({
   configSection,
   depth,
   resource,
 }: SectionRendererProps) {
-  return (
-    <div
-      key={configSection.display}
-      className={`p-2 bg-gray-${100 * depth} rounded-xl ${depth == 0 && 'flex flex-col gap-2'}`}
-    >
-      {configSection.display && <h3>{configSection.display}</h3>}
-      {configSection.components.map((component) => (
-        <ComponentRenderer
-          key={uuidv4()}
-          configComponent={component}
-          resource={resource}
-          depth={depth + 1}
-        />
-      ))}
-    </div>
-  );
+  const sections = configSection.components.map((component) => {
+    return (
+      <ComponentRenderer
+        key={uuidv4()}
+        configComponent={component}
+        resource={resource}
+        depth={depth + 1}
+      />
+    );
+  });
+
+  const filteredSections = sections.filter((section) => !isEmptyDiv(section));
+
+  if (filteredSections.length > 0) {
+    return (
+      <div
+        key={configSection.display}
+        className={`p-2 bg-gray-${100 * depth} rounded-xl ${depth == 0 && 'flex flex-col gap-2'}`}
+      >
+        {configSection.display && <h3>{configSection.display}</h3>}
+        {filteredSections}
+      </div>
+    );
+  }
+
+  return null;
 }
