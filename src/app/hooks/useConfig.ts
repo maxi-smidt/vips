f759cffd29286c0e64ccf2481cf642bc062a913b
msmidt <maxismidt@icloud.com>
2024-10-28 10:43:42 +0100

    [Feature #35] custom sections

--- src/app/hooks/useConfig.ts
+++ src/app/hooks/useConfig.ts
@@ -1,22 +1,59 @@
 'use client';
 
 import { Config } from '../types/Config';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { DefaultConfigEN } from '@/app/config/default_config_en';
+import { useToast } from '@/app/hooks/useToast';
+import { ConfigContext } from '@/app/provider/ConfigProvider';
 
 export default function useConfig() {
-  const [config, setConfig] = useState<Config>(DefaultConfigEN);
+  const { showError } = useToast();
+  const { config, defaultConfig, setConfig } = useContext(ConfigContext);
 
   useEffect(() => {
+    loadCustomConfig(false);
+  }, []);
+
+  const loadCustomConfig = (showMessage: boolean = true) => {
     if (typeof window !== 'undefined') {
       const customConfig = localStorage.getItem('customConfig');
       if (customConfig) {
-        setConfig(JSON.parse(customConfig));
+        let parsedConfig = undefined;
+        try {
+          parsedConfig = JSON.parse(customConfig);
+          setConfig(parsedConfig);
+        } catch (_) {
+          if (showMessage) showError('Custom configuration format was no JSON');
+        }
+      } else {
+        if (showMessage) showError('No custom configuration found');
       }
     }
-  }, []);
+  };
+
+  const saveCustomConfig = (config: Config) => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('customConfig', JSON.stringify(config));
+    }
+  };
+
+  const clearCustomConfig = () => {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('customConfig');
+      setConfig(DefaultConfigEN);
+    }
+  };
+
+  const loadDefaultConfig = () => {
+    setConfig(DefaultConfigEN);
+  };
 
   return {
     config,
+    defaultConfig,
+    loadCustomConfig,
+    loadDefaultConfig,
+    clearCustomConfig,
+    saveCustomConfig,
   };
 }
