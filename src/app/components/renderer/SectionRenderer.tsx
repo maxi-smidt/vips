c5e66f7dee06f90cd6c525720cb79a8674ea90fa
msmidt <maxismidt@icloud.com>
2024-10-31 20:27:39 +0100

    [Feature #137] refactoring

--- src/app/components/renderer/SectionRenderer.tsx
+++ src/app/components/renderer/SectionRenderer.tsx
@@ -1,4 +1,4 @@
-import { ConfigSection, isConfigEntry } from '@/app/types/Config';
+import { ConfigSection } from '@/app/types/Config';
 import React from 'react';
 import ComponentRenderer from '@/app/components/renderer/ComponentRenderer';
 import { v4 as uuidv4 } from 'uuid';
@@ -37,10 +37,12 @@ export default function SectionRenderer({
     return (
       <div
         key={configSection.display}
-        className={`p-2 bg-gray-${100 * depth} rounded-xl ${depth == 0 && 'flex flex-col gap-2'}`}
+        className={`p-2 bg-gray-${100 * depth} rounded-xl`}
       >
         {configSection.display && <h3>{configSection.display}</h3>}
-        <div className="ml-4">{filteredSections}</div>
+        <div className={`${depth === 0 ? 'flex flex-col gap-2' : 'ml-4'}`}>
+          {filteredSections}
+        </div>
       </div>
     );
   }
